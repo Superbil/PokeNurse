4c4c0a5a08e859842725974db484b1921ae5a371
Mack Hankins <mack.hankins@gmail.com>
2016-08-11 08:09:57 -0500

    css fixes

--- js/screens/Table/components/Pokemon.jsx
+++ js/screens/Table/components/Pokemon.jsx
@@ -1,44 +1,19 @@
 import React from 'react'
 import renderModal from '../../Detail'
 
-function prepDisplay(d) {
-    for (var i = 0; i < d.pokemon.length; i++) {
-        var poke = d.pokemon[i]
-        var checkBox = '<input type="checkbox" value="' + poke.id.toString() + '"'
-        var favorite = 'glyphicon glyphicon-star-empty'
-        var pokeiv = poke['iv'] + '% (' + poke['attack'] + '/' + poke['defense'] + '/' + poke['stamina'] + ')'
-        var favoriteBool = poke['favorite'] ? 'true' : 'false'
-
-        if (poke.deployed) checkBox += ' disabled'
-        if (poke.favorite) favorite = 'glyphicon glyphicon-star favorite-yellow'
-
-        poke.td_checkbox = checkBox + '>'
-        poke.td_favorite = '<span class="favorite ' + favorite + '" id="favoriteBtn" data-pokemon-id="' + poke.id + '" data-pokemon-favorited="' + favoriteBool + '" />'
-        poke.td_name = poke.name
-        poke.td_nickname = '<a class="nickname" data-pokemon-id="' + poke.id + '">' + poke.nickname + '</a>'
-        poke.td_cp = poke.cp
-        poke.td_pokeiv = pokeiv
-    }
-
-    return d.pokemon
-}
-
 const Pokemon = React.createClass({
-
     render () {
-        let {
-            species
-        } = this.props
+        let {species} = this.props
 
         return (
-            <tr className="child" key={'sub' + species.pokemon_id}>
-                <td colSpan="6">
-                    <table className="table table-condensed table-hover">
+            <tr className='child' key={'sub' + species.pokemon_id}>
+                <td colSpan='7'>
+                    <table className='table table-condensed table-hover'>
                         <thead>
                         <tr>
                             <th></th>
                             <th>
-                                Favorite
+                                <span className='glyphicon glyphicon-star favorite-yellow'></span>
                             </th>
                             <th>
                                 Name
@@ -64,27 +39,56 @@ const Pokemon = React.createClass({
     },
 
     getPokemonComponents (species) {
-      return prepDisplay(species).map( (pokemon) => {
-        return (
-          <tr key={pokemon.id}>
-            <td>
-              <span dangerouslySetInnerHTML={{__html: pokemon.td_checkbox}}></span>
-            </td>
-            <td>
-              <span dangerouslySetInnerHTML={{__html: pokemon.td_favorite}}></span>
-            </td>
-            <td>{pokemon.td_name}</td>
-            <td onClick = {this.handleClickNickname.bind(this, pokemon, species)}>
-              <span dangerouslySetInnerHTML={{__html: pokemon.td_nickname}}></span>
-            </td>
-            <td>{pokemon.td_cp}</td>
-            <td>{pokemon.td_pokeiv}</td>
-          </tr>)
-      })
+        return species.pokemon.map((pokemon) => {
+            let favorite = pokemon.favorite ? 'glyphicon glyphicon-star favorite-yellow' : 'glyphicon glyphicon-star-empty'
+            let favoriteBool = pokemon.favorite ? 'true' : 'false'
+            let pokeiv = pokemon['iv'] + '% (' + pokemon['attack'] + '/' + pokemon['defense'] + '/' + pokemon['stamina'] + ')'
+
+            return (
+                <tr key={pokemon.id}>
+                    <td>
+                        <input
+                            type='checkbox'
+                            value={String(pokemon.id)}
+                            disabled={pokemon.deployed}
+                        />
+                    </td>
+                    <td>
+            <span
+                className={`favorite ${favorite}`}
+                id='favoriteBtn'
+                data-pokemon-id={pokemon.id}
+                data-pokemon-favorited={favoriteBool}
+                onClick={this.handleClickFavorite.bind(this, pokemon)}
+            />
+                    </td>
+                    <td>
+                        {pokemon.name}
+                    </td>
+                    <td onClick={this.handleClickNickname.bind(this, pokemon, species)}>
+                        <a
+                            className='nickname'
+                            data-pokemon-id={pokemon.id}
+                        >
+                            {pokemon.nickname}
+                        </a>
+                    </td>
+                    <td>
+                        {pokemon.cp}
+                    </td>
+                    <td>
+                        {pokeiv}
+                    </td>
+                </tr>)
+        })
+    },
+
+    handleClickFavorite (pokemon, e) {
+        console.log(pokemon)
     },
 
     handleClickNickname (pokemon, species, e) {
-      renderModal($(document.getElementById('detailModal')), pokemon, species)
+        renderModal($(document.getElementById('detailModal')), pokemon, species)
     }
 
 })
